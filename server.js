d069d77d505aea9b8939b714e942a06205c92fd2
David <davidmorano91@gmail.com>
2019-03-13 10:27:20 +0100

    Redirigir añadido

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ mongoose.connect(config.db_uri,{useNewUrlParser:true})
 .catch(err => console.log('Error en la conexión a la BD'));
 
 // --- MIDDLEWARE
+// Para redirigir trafico HTTP a HTTPS
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else
+      next();
+  });
 //Rutas
 app.use(express.json());
 app.use('/api',routes);
